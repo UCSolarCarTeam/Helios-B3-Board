beefd50eb4e58945be3914e484cb810a4b92834b
Christopher Chan <cjchan.chris@gmail.com>
2023-11-01 17:52:50 -0600

    Initial system

---
+++ Components/Core/Inc/RunInterface.hpp
@@ -0,0 +1,18 @@
+/*
+ * RunInterface.hpp
+ *
+ *  Created on: Apr 3, 2023
+ *      Author: Chris (cjchanx)
+ */
+
+#ifndef C__IFACE_HPP_
+#define C__IFACE_HPP_
+
+void run_interface();
+
+void cpp_USART1_IRQHandler();
+void cpp_USART2_IRQHandler();
+void cpp_USART3_IRQHandler();
+void cpp_USART5_IRQHandler();
+
+#endif /* C__IFACE_HPP_ */
---
+++ Components/Core/RunInterface.cpp
@@ -0,0 +1,24 @@
+/*
+ *  RunInterface.cpp
+ *
+ *  Created on: Apr 3, 2023
+ *      Author: Chris (cjchanx)
+ */
+
+#include "main_system.hpp"
+#include "UARTDriver.hpp"
+
+extern "C" {
+    void run_interface()
+    {
+        run_main();
+    }
+
+    void cpp_USART5_IRQHandler()
+    {
+        Driver::uart5.HandleIRQ_UART();
+    }
+}
+
+
+
